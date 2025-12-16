1b7b05fce43df2c0535672f75426c1245e4f0433
kamran <147907811+Code-with-kamran@users.noreply.github.com>
2025-12-16 23:35:22 +0500

    helllllo

--- DentalClicnicManagementSystem/obj/Debug/net8.0/CMS.AssemblyInfo.cs
+++ DentalClicnicManagementSystem/obj/Debug/net8.0/CMS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CMS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31e440e09472880c5630db68239b38c6db79dfbd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0ab2551568ca280aa402812d60977806a3dc10a")]
 [assembly: System.Reflection.AssemblyProductAttribute("CMS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CMS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
